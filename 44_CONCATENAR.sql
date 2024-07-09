6b6797a6dacf149f4723765a04c03b228dc939e8
Cesar.Sotnas <devcarlos.cesar@gmail.com>
2024-07-08 22:46:32 -0300

    Fundamentos de SQL

---
+++ 44_CONCATENAR.sql
@@ -0,0 +1,11 @@
+USE ContosoRetailDW
+
+DECLARE @USER VARCHAR(30)
+SET @USER = ' Maria Maria '
+
+DECLARE @USER2 VARCHAR(30)
+SET @USER2 = ' João João '
+
+SELECT @USER + @USER2 AS NAMES
+
+SELECT CONCAT('Cesar Santos', @USER, @USER2)
