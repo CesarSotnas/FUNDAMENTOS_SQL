18813f8e5e61c13a8598c1b552dbf716125b3ba2
Cesar.Sotnas <devcarlos.cesar@gmail.com>
2024-07-10 23:36:37 -0300

    Fundamentos de SQL

---
+++ 45_SUBSTITUICAO_DE_TEXTO.sql
@@ -0,0 +1,6 @@
+
+
+SELECT 
+	' SQL para analise de dados',
+	REPLACE(' SQL para analise de dados', 'analise', 'análise'), -- ORIGINAL, O QUE QUER ENCONTRAR, O QUE QUER SUBSTITUIR
+	REPLACE(' SQL para analise de dados', 'de dados', '') -- ORIGINAL, O QUE QUER ENCONTRAR, O QUE QUER SUBSTITUIR
