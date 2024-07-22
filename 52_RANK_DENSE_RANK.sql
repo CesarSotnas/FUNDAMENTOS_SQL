7eed941638ef18fd7bc415d0ea9f1a13ebae6ff3
Cesar.Sotnas <devcarlos.cesar@gmail.com>
2024-07-21 22:53:41 -0300

    Fundamentos de SQL

---
+++ 52_RANK_DENSE_RANK.sql
@@ -0,0 +1,11 @@
+USE Chinook
+
+SELECT [TrackId]
+      ,[Name]
+	  ,[AlbumId]
+      ,[Milliseconds]
+      ,[UnitPrice]
+	  , ROW_NUMBER() OVER(ORDER BY Milliseconds ASC) AS 'ROW_NUMBER'
+  	  , RANK() OVER(ORDER BY UnitPrice ASC) AS 'RANK'
+	  , DENSE_RANK() OVER(ORDER BY UnitPrice ASC) AS 'DENSE_RANK'
+  FROM [Chinook].[dbo].[Track]
